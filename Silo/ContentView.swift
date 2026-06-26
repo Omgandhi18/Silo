24e2b1dad29a0696f77383a13808138886f81c8f
om <omgandhi255@gmail.com>
2026-06-26 12:04:17 +0530

    Initial Commit

---
+++ Silo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Silo
+//
+//  Created by Om Gandhi on 26/06/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Silo/SiloApp.swift
@@ -0,0 +1,17 @@
+//
+//  SiloApp.swift
+//  Silo
+//
+//  Created by Om Gandhi on 26/06/26.
+//
+
+import SwiftUI
+
+@main
+struct SiloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
